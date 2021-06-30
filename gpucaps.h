704f7be8597cb2c42dc04631cf1a7252c5273a1b
v.koda <v.koda@dragonlk.com>
2021-07-01 01:28:04 +0300

    Implemented printLn(), printEndLn() and printHeading() functions

--- gpucaps.h
+++ gpucaps.h
@@ -1,3 +1,5 @@
+static std::streamsize fieldWidth = 0;
+
 inline std::string versionString(uint32_t version)
 {
     uint32_t major = (version >> 22) & 0b1111111111; // 10
@@ -32,4 +34,35 @@ template<typename Bool>
 inline std::string booleanString(Bool value)
 {
     return value ? "Yes" : "No";
+}
+
+inline void setFieldWidth(std::streamsize width)
+{
+    fieldWidth = width;
+}
+
+template<typename Type>
+inline void printLn(const char *description, const Type& value)
+{
+    std::cout << std::setw(fieldWidth) << std::left << description << value << std::endl;
+}
+
+inline void printEndLn()
+{
+    std::cout << std::endl;
+}
+
+inline void printHeading(const char *description)
+{
+    const std::size_t length = strlen(description);
+    std::size_t dashedLength = (80 - length)/2;
+    printEndLn();
+    for (std::size_t i = 0; i < dashedLength - 1; ++i)
+        std::cout << "=";
+    std::cout << " " << description << " ";
+    if (length % 2)
+        ++dashedLength;
+    for (std::size_t i = 1; i < dashedLength; ++i)
+        std::cout << "=";
+    printEndLn();
 }
